5cfebd72a4d6a7d76b54593be0825f969af18822
lIlIIIlIIIl <102349883+lIlIIIlIIIl@users.noreply.github.com>
2023-09-03 23:20:59 +0900

    add quiz&leaderboard form

--- lib/constants/gaps.dart
+++ lib/constants/gaps.dart
@@ -8,8 +8,11 @@ final gap34w = SizedBox(width: 34.w);
 final gap106w = SizedBox(width: 106.w);
 
 // vertical gap
+final gap6h = SizedBox(height: 6.h);
 final gap15h = SizedBox(height: 15.h);
 final gap30h = SizedBox(height: 30.h);
+final gap31h = SizedBox(height: 31.h);
+final gap43h = SizedBox(height: 43.h);
 final gap50h = SizedBox(height: 50.h);
 final gap64h = SizedBox(height: 64.h);
 final gap70h = SizedBox(height: 70.h);
--- lib/constants/palette.dart
+++ lib/constants/palette.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Palette {
   static const purple10 = Color(0xff21005d);
   static const purple40 = Color(0xff6750a4);
-  static const purple50 = Color(0xff7f67be);
+  static const purple50 = Color(0xff7D63C3);
   static const purple60 = Color(0xff9A82DB);
   static const purple80 = Color(0xFFD0BCFF);
   static const purple95 = Color(0xfff6edff);
